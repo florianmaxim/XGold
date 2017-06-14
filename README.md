22be592292cf047b8553c2d3fabfc521827e867b
Florian Maxim <look@cheesyeyes.com>
2017-06-14 12:04:55 +0200

    its a paly alpha

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Let's extract gold.
 
+Every Block is a diamond.
+
 ![Gold](stills/1.png)
--- source/components/Block.js
+++ source/components/Block.js
@@ -6,6 +6,27 @@ import Gold from './Gold';
 
 import ModelBlock from './ModelBlock';
 
+const isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
 const DEFAULT = {
   line: 'Targold',
   input: ':number'
@@ -28,26 +49,52 @@ export default class Block extends React.Component{
     }
   }
 
-  handleNumpad(){
+  componentWillMount(){}
+
+  componentDidMount(){
+    addEventListener('touchstart', (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+    });
+
+    addEventListener('resize', (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      this.forceUpdate();
+    });
+  }
+
+  handleNumpad(event){
+    event.stopPropagation();
+    event.preventDefault();
+
     this.setState({
       numpad: this.state.numpad?false:true
     })
-    this.forceUpdate();
   }
 
-  handleInput(v){
+  handleInput(v, event){
+    event.stopPropagation();
+    event.preventDefault();
+
     this.setState({
       input:this.state.input!=DEFAULT.input?(this.state.input+v.toString()):v.toString()
     })
   }
 
-  handleBackspace(){
+  handleBackspace(event){
+    event.stopPropagation();
+    event.preventDefault();
+
     this.setState({
       input:this.state.input.substring(0, this.state.input.length - 1)
     })
   }
 
-  handleGold(){
+  handleGold(event){
+    event.stopPropagation();
+    event.preventDefault();
+
     this.setState({
       gold: this.state.gold?false:true,
       hash: this.block.get(this.state.input)
@@ -56,48 +103,76 @@ export default class Block extends React.Component{
   }
 
   render(){
+
       return(
-        <div className="home">
-          <div className="home-gold">
+        <div className="block" style={{height: isMobile.iOS?'calc(100vh - 44px)':'100vh'}}>
+          <div className="block-gold">
             {this.state.gold? <Gold input={this.state.input}/>: ''}
           </div>
           {this.state.numpad
             ?
-            <div className="home-interface">
-              <div className="home-input">
+            <div className="block-interface">
+              <div className="block-input">
                 {this.state.input}_
               </div>
-              <div className="numpad">
-               <div className="numpad-row">
-                <div className="numpad-number" onClick={()=>{this.handleInput(1)}}>1</div>
-                <div className="numpad-number" onClick={()=>{this.handleInput(2)}}>2</div>
-                <div className="numpad-number" onClick={()=>{this.handleInput(3)}}>3</div>
-               </div>
-               <div className="numpad-row">
-                <div className="numpad-number" onClick={()=>{this.handleInput(4)}}>4</div>
-                <div className="numpad-number" onClick={()=>{this.handleInput(5)}}>5</div>
-                <div className="numpad-number" onClick={()=>{this.handleInput(6)}}>6</div>
-               </div>
-               <div className="numpad-row">
-                <div className="numpad-number" onClick={()=>{this.handleInput(7)}}>7</div>
-                <div className="numpad-number" onClick={()=>{this.handleInput(8)}}>8</div>
-                <div className="numpad-number" onClick={()=>{this.handleInput(9)}}>9</div>
-               </div>
-               <div className="numpad-row">
-                <div className="numpad-number-secondary" onClick={()=>{this.handleBackspace()}}>back</div>
-                <div className="numpad-number" onClick={()=>{this.handleInput(0)}}>0</div>
-                <div className="numpad-number-secondary" onClick={()=>{this.handleGold(0)}}>gold</div>
-               </div>
-              </div>
+
+               {innerWidth<innerHeight
+                 ?
+                 /* Portrait numpad */
+                 <div className="numpad">
+                  <div className="numpad-row">
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(1, event)}} onClick={(event)=>{this.handleInput(1, event)}}>1</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(2, event)}} onClick={(event)=>{this.handleInput(2, event)}}>2</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(3, event)}} onClick={(event)=>{this.handleInput(3, event)}}>3</div>
+                  </div>
+                  <div className="numpad-row">
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(4, event)}} onClick={(event)=>{this.handleInput(4, event)}}>4</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(5, event)}} onClick={(event)=>{this.handleInput(5, event)}}>5</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(6, event)}} onClick={(event)=>{this.handleInput(6, event)}}>6</div>
+                  </div>
+                  <div className="numpad-row">
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(7, event)}} onClick={(event)=>{this.handleInput(7, event)}}>7</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(8, event)}} onClick={(event)=>{this.handleInput(8, event)}}>8</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(9, event)}} onClick={(event)=>{this.handleInput(9, event)}}>9</div>
+                  </div>
+                  <div className="numpad-row">
+                   <div className="numpad-number" style={{backgroundColor:'gold', color:'black'}} onTouchStart={(event)=>{this.handleBackspace(event)}} onClick={(event)=>{this.handleBackspace(event)}}>⌥</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(0, event)}} onClick={(event)=>{this.handleInput(0, event)}}>0</div>
+                   <div className="numpad-number" style={{backgroundColor:'gold', color:'black'}} onTouchStart={(event)=>{this.handleGold(event)}} onClick={(event)=>{this.handleGold(event)}}>⌘</div>
+                  </div>
+                 </div>
+
+                 :
+                 /* Landscape numpad */
+                 <div className="numpad">
+                  <div className="numpad-row">
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(1, event)}} onClick={(event)=>{this.handleInput(1, event)}}>1</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(2, event)}} onClick={(event)=>{this.handleInput(2, event)}}>2</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(3, event)}} onClick={(event)=>{this.handleInput(3, event)}}>3</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(4, event)}} onClick={(event)=>{this.handleInput(4, event)}}>4</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(5, event)}} onClick={(event)=>{this.handleInput(5, event)}}>5</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(6, event)}} onClick={(event)=>{this.handleInput(6, event)}}>6</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(7, event)}} onClick={(event)=>{this.handleInput(7, event)}}>7</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(8, event)}} onClick={(event)=>{this.handleInput(8, event)}}>8</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(9, event)}} onClick={(event)=>{this.handleInput(9, event)}}>9</div>
+                  </div>
+                  <div className="numpad-row">
+                   <div className="numpad-number" style={{backgroundColor:'gold', color:'black'}} onTouchStart={(event)=>{this.handleBackspace(event)}} onClick={(event)=>{this.handleBackspace(event)}}>⌥</div>
+                   <div className="numpad-number" onTouchStart={(event)=>{this.handleInput(0, event)}} onClick={(event)=>{this.handleInput(0, event)}}>0</div>
+                   <div className="numpad-number" style={{backgroundColor:'gold', color:'black'}} onTouchStart={(event)=>{this.handleGold(event)}} onClick={(event)=>{this.handleGold(event)}}>⌘</div>
+                  </div>
+                 </div>
+               }
+
             </div>
             :
-            <div className="home-interface">
-              <div className="home-line">
+            <div className="block-interface">
+              <div className="block-line" onTouchEnd={(event)=>{this.handleNumpad(event)}} onClick={()=>{this.handleNumpad(event)}}>
                 {this.state.hash}
               </div>
             </div>
           }
-          <div className="home-logo" onClick={this.handleNumpad.bind(this)}>
+          <div className="block-logo" onTouchEnd={(event)=>{this.handleNumpad(event)}} onClick={()=>{this.handleNumpad(event)}}>
             <Logo/>
           </div>
         </div>
