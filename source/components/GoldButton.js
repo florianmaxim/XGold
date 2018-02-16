365c9e364383788697184ec6130bf7a2973f4e76
florianmaxim <hello@florianmaxim.com>
2018-02-16 21:03:59 +0100

    removed gold button circle

--- source/components/GoldButton.js
+++ source/components/GoldButton.js
@@ -3,11 +3,11 @@ import React, {Component} from 'react';
 const GoldButton = () =>
 
 <div className="treasure">
-    <div className="gold-circle">
-        <div className="gold-outer">
-            <div className="gold-inner"/>
-        </div>
-    </div>    
+    
+    <div className="gold-outer">
+        <div className="gold-inner"/>
+    </div>
+    
 </div>;
 
 export default GoldButton;
