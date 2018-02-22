563009505bb7065fc25412d0c7aa00446bcc5d1a
florianmaxim <hello@florianmaxim.com>
2018-02-22 12:53:39 +0100

    Changed HTML Header

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ app.get('*', (req, res) => {
   
   }else{
 
-    res.write(`<!doctype html><html><head> <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title></title>${styles}</head><body><div id="root">${html}</div><script src="/bundle.js"></script></body></html>`)
+    res.write(`<!doctype html><html><head><meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title></title>${styles}</head><body><div id="root">${html}</div><script src="/bundle.js"></script></body></html>`)
     res.end();
     
   }
