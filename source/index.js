6e94aac75c9ad28aefd0e400e10cb2e3542409fb
florianmaxim <hello@florianmaxim.com>
2018-02-26 12:57:16 +0100

    Tabula raza

--- source/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Root from './Root';
-
-import './styles/index.scss';
-
-render(<Root />, document.getElementById('root'));
-
-if (module.hot) module.hot.accept();
